7570f33f6855c8331344944a28c6e706262f6c9b
kitakun <kitakunx@gmail.com>
2020-02-02 16:32:57 +0700

    Remove unsuccessfull attemps to use Execute or StoreProcedures on vk side

--- Kitakun.VkModules.Services.GroupLikeService/GroupLikesService.cs
+++ Kitakun.VkModules.Services.GroupLikeService/GroupLikesService.cs
@@ -1,132 +1,203 @@
 namespace Kitakun.VkModules.Services.GroupLikeService
 {
-	using System;
-	using System.Linq;
-	using System.Collections.Generic;
-	using System.Threading.Tasks;
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
 
-	using VkNet;
-	using VkNet.Enums.Filters;
-	using VkNet.Model.RequestParams;
-	using VkNet.Model;
-	using VkNet.Enums.SafetyEnums;
-	using VkNet.Model.Attachments;
+    using VkNet;
+    using VkNet.Enums.Filters;
+    using VkNet.Model.RequestParams;
+    using VkNet.Model;
+    using VkNet.Enums.SafetyEnums;
+    using VkNet.Model.Attachments;
 
-	using Kitakun.VkModules.Services.Abstractions;
+    using Kitakun.VkModules.Services.Abstractions;
+    using Kitakun.VkModules.Services.GroupLikeService.Models;
+    using System.Collections.ObjectModel;
 
     // https://vk.com/dev/objects/appWidget
     internal class GroupLikesService : IGroupLikesService
-	{
-		private VkApi _sharedVkApi;
-		private string _sharedForToken;
+    {
+        private const ulong loadPerPart = 100;
 
-		public async Task<VkApi> GetApi(string token)
-		{
-			if(_sharedVkApi != null && _sharedForToken == token)
-			{
-				return _sharedVkApi;
-			}
+        private VkApi _sharedVkApi;
+        private string _sharedForToken;
 
-			var api = new VkApi();
+        public async Task<VkApi> GetApi(string token)
+        {
+            if (_sharedVkApi != null && _sharedForToken == token)
+            {
+                return _sharedVkApi;
+            }
 
-			await api.AuthorizeAsync(new ApiAuthParams
-			{
-				AccessToken = token,
-				Settings = Settings.All
-			});
-			_sharedForToken = token;
-			_sharedVkApi = api;
+            var api = new VkApi();
+
+            await api.AuthorizeAsync(new ApiAuthParams
+            {
+                AccessToken = token,
+                Settings = Settings.All
+            });
+            _sharedForToken = token;
+            _sharedVkApi = api;
 
             return api;
-		}
+        }
 
-		public async Task<IDictionary<long, int>> LoadAllLikesForCommunityPostsAsync(
-			string token,
-			long userId,
-			DateTime from,
-			DateTime to)
-		{
-			var api = await GetApi(token);
+        public async Task<IDictionary<long, int>> LoadAllLikesForCommunityPostsAsync(
+            string token,
+            long userId,
+            DateTime from,
+            DateTime to)
+        {
+            var api = await GetApi(token);
 
-			var allPosts = new List<Post>();
+            var allPosts = new List<LoadWallPostsStoreProcedureRecord>(64);
 
-            const ulong loadPerPart = 100;
+            var lastLoadedPosts = await api.Wall.GetAsync(new WallGetParams
+            {
+                Count = loadPerPart,
+                OwnerId = userId,
+                Extended = false
+            });
 
-			var lastLoadedPosts = await api.Wall.GetAsync(new WallGetParams
-			{
-				Count = loadPerPart,
-				OwnerId = userId,
-				Extended = false
-			});
-			//validating first recieve posts
-			var firstPosts = lastLoadedPosts.WallPosts.Where(a => a.Date > from && a.Date < to);
-			allPosts.AddRange(firstPosts);
+            //validating first recieve posts
+            proceedPostsToAllPostsCollection(lastLoadedPosts.WallPosts);
 
-			ulong loadedCount = 0;
+            ulong loadedCount = 0;
 
-			var loadMore = lastLoadedPosts.TotalCount > loadPerPart;
-			while (loadMore)
-			{
-				loadedCount += loadPerPart;
-				lastLoadedPosts = await api.Wall.GetAsync(new WallGetParams
-				{
-					Count = loadPerPart,
-					OwnerId = userId,
-					Extended = false,
+            void proceedPostsToAllPostsCollection(ReadOnlyCollection<Post> posts)
+            {
+                for (var i = 0; i < posts.Count; i++)
+                {
+                    var curPost = posts[i];
+                    if (curPost.Date > from && curPost.Date < to)
+                    {
+                        allPosts.Add(new LoadWallPostsStoreProcedureRecord
+                        {
+                            Id = curPost.Id.Value,
+                            Likes = curPost.Likes.Count,
+                            Comments = curPost.Comments.Count,
+                            Reposts = curPost.Reposts.Count
+                        });
+                    }
+                }
+            }
+
+            var loadMore = lastLoadedPosts.TotalCount > loadPerPart;
+            while (loadMore)
+            {
+                loadedCount += loadPerPart;
+                lastLoadedPosts = await api.Wall.GetAsync(new WallGetParams
+                {
+                    Count = loadPerPart,
+                    OwnerId = userId,
+                    Extended = false,
                     Offset = loadedCount
-				});
-				allPosts.AddRange(lastLoadedPosts.WallPosts.Where(a => a.Date > from && a.Date < to));
-				loadMore = lastLoadedPosts.WallPosts.Count > 0 
-                    && !lastLoadedPosts.WallPosts.Any(a => a.Date >= from && a.Date <= to) || lastLoadedPosts.TotalCount > loadedCount;
-			}
+                });
 
-			var linkedUserWithLikesCount = new Dictionary<long, int>();
+                proceedPostsToAllPostsCollection(lastLoadedPosts.WallPosts);
 
-			if (allPosts.Count > 0)
-			{
-				var postsIdsWithLikes = allPosts
-					.Where(w => w.Likes.Count > 0 && w.Id.HasValue)
-					.Select(s => s.Id.Value)
-                    .ToArray();
+                loadMore = lastLoadedPosts.WallPosts.Count > 0;
+                if (loadMore)
+                {
+                    for (var i = 0; i < lastLoadedPosts.WallPosts.Count; i++)
+                    {
+                        var post = lastLoadedPosts.WallPosts[i];
+                        var anyCondition = post.Date >= from && post.Date <= to
+                            || lastLoadedPosts.TotalCount > loadedCount;
+                        loadMore = !anyCondition;
+                        if (!loadMore)
+                            break;
+                    }
+                }
+            }
 
-				var loadPostsTask = postsIdsWithLikes
-					.Select(postId => api.Likes.GetListAsync(new LikesGetListParams
-					{
-						OwnerId = userId,
-						Type = LikeObjectType.Post,
-						ItemId = postId,
-						Extended = true
-					})).ToArray();
+            var linkedUserWithLikesCount = new Dictionary<long, int>();
 
-				await Task.WhenAll(loadPostsTask);
+            if (allPosts.Count > 0)
+            {
+                var postsIdsWithLikes = CreatePostsIdsWithLikes(allPosts);
 
-				linkedUserWithLikesCount = loadPostsTask
-					.Select(s => s.Result)
-					.SelectMany(s => s)
-					.GroupBy(v => v)
-					.ToDictionary(s => s.Key, v => v.Count());
-			}
+                if (postsIdsWithLikes.Length > 0)
+                {
+                    var loadPostsTask = postsIdsWithLikes
+                        .Select(postId => api.Likes.GetListAsync(new LikesGetListParams
+                        {
+                            OwnerId = userId,
+                            Type = LikeObjectType.Post,
+                            ItemId = postId,
+                            Extended = true
+                        })).ToArray();
 
-			return linkedUserWithLikesCount;
-		}
+                    await Task.WhenAll(loadPostsTask);
 
-		public async Task<IDictionary<long, (string imgUrl, string uName)>> GetUsersPhotoAndNames(string token, long[] userIds)
-		{
-			var api = await GetApi(token);
+                    FillDictionaryWithLikes(ref linkedUserWithLikesCount, loadPostsTask);
+                }
+            }
 
-			IDictionary<long, (string imgUrl, string uName)> methodResponse = api
-				.Users
-				.Get(userIds, ProfileFields.Photo50)
-				.Select(s => new
-				{
-					s.Id,
-					s.Photo50,
-					s.FirstName,
-					s.LastName
-				})
-				.ToDictionary(k => k.Id, v => (imgUrl: v.Photo50.ToString(), uName: $"{v.FirstName} {v.LastName}"));
+            return linkedUserWithLikesCount;
+        }
 
-			return methodResponse;
-		}
-	}
+        public async Task<IDictionary<long, (string imgUrl, string uName)>> GetUsersPhotoAndNames(string token, long[] userIds)
+        {
+            var api = await GetApi(token);
+
+            IDictionary<long, (string imgUrl, string uName)> methodResponse = api
+                .Users
+                .Get(userIds, ProfileFields.Photo50)
+                .Select(s => new
+                {
+                    s.Id,
+                    s.Photo50,
+                    s.FirstName,
+                    s.LastName
+                })
+                .ToDictionary(k => k.Id, v => (imgUrl: v.Photo50.ToString(), uName: $"{v.FirstName} {v.LastName}"));
+
+            return methodResponse;
+        }
+
+        private static long[] CreatePostsIdsWithLikes(List<LoadWallPostsStoreProcedureRecord> allPosts)
+        {
+            var preCount = 0;
+            for (var i = 0; i < allPosts.Count; i++)
+            {
+                if (allPosts[i].Likes > 0)
+                {
+                    preCount++;
+                }
+            }
+            var resultArray = new long[preCount];
+            var index = 0;
+            for (var i = 0; i < allPosts.Count; i++)
+            {
+                if (allPosts[i].Likes > 0)
+                {
+                    resultArray[index] = allPosts[i].Id;
+                }
+                index++;
+            }
+            return resultArray;
+        }
+
+        private static void FillDictionaryWithLikes(ref Dictionary<long, int> linkedUserWithLikesCount, Task<VkNet.Utils.VkCollection<long>>[] loadPostsTask)
+        {
+            for (var i = 0; i < loadPostsTask.Length; i++)
+            {
+                var taskResult = loadPostsTask[i].Result;
+                for (var j = 0; j < taskResult.Count; j++)
+                {
+                    if (linkedUserWithLikesCount.ContainsKey(taskResult[j]))
+                    {
+                        linkedUserWithLikesCount[taskResult[j]] = linkedUserWithLikesCount[taskResult[j]] + 1; 
+                    }
+                    else
+                    {
+                        linkedUserWithLikesCount.Add(taskResult[j], 1);
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ Kitakun.VkModules.Services.GroupLikeService/Models/LoadWallPostsStoreProcedureRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace Kitakun.VkModules.Services.GroupLikeService.Models
+{
+    public struct LoadWallPostsStoreProcedureRecord
+    {
+        public long Id { get; set; }
+        public long Likes { get; set; }
+        public long Comments { get; set; }
+        public long Reposts { get; set; }
+    }
+}
